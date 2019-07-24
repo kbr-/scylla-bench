ded10079783f20716f5cbf5d705e05a0f7545cc5
Kamil Braun <kbraun@scylladb.com>
2019-07-24 17:55:29 +0200

    measure reactor stalls

--- bench-compr.py
+++ bench-compr.py
@@ -7,6 +7,8 @@ import os
 import time
 import subprocess
 import json
+import re
+import statistics as stats
 
 def read_chunks(f, sz):
     while True:
@@ -30,7 +32,7 @@ LZ4 = ('LZ4', 'LZ4Compressor')
 SNAPPY = ('Snappy', 'SnappyCompressor')
 DEFLATE = ('Deflate', 'DeflateCompressor')
 ZSTD = ('Zstd', 'ZstdCompressor')
-ZSTD_LEVELS = list(range(1, 23, 4))
+ZSTD_LEVELS = [1, 3, 5, 7, 9]
 COMPR_CHUNKS = [4, 16, 64]
 
 # [(short name, long name, level)]
@@ -44,13 +46,36 @@ def compressions(chunk):
 
 SCYLLA_HOME = '/home/kbraun/dev/scylla/'
 
-HOW_MUCH = 100 * 1024 * 1024
+HOW_MUCH = 10 * 1024 * 1024
 CHUNK_SIZE = 1024
 
 KS_DIR = os.path.join(SCYLLA_HOME, 'tmp/test_ks')
 
-# Takes a file name, compression dict, returns (time, space)
-def bench_compr(fname, cdict):
+if len(sys.argv) < 4:
+    print("Requires data file name, output file name, Scylla's stderr file name")
+    sys.exit(0)
+
+infname = sys.argv[1]
+outfname = sys.argv[2]
+logfname = sys.argv[3]
+
+for fname in [infname, outfname, logfname]:
+    if not os.path.exists(fname):
+        print("File does not exist:", fname)
+        sys.exit(0)
+
+def consume_stalls(f):
+    res = []
+    while True:
+        line = f.readline()
+        if not line:
+            return res
+        ms = re.match(r"Reactor stalled for (\d+) ms.*", line)
+        if ms:
+            res.append(int(ms.group(1)))
+
+# Takes a file name, compression dict, handle to file with Scylla's stderr, returns (time, space, stalls)
+def bench_compr(fname, cdict, logf):
     with Cluster() as cluster:
         session = cluster.connect('test_ks')
 
@@ -83,39 +108,34 @@ def bench_compr(fname, cdict):
         sys.stdout.flush()
 
         print("Flushing...")
+        logf.seek(0, 2)
         t1 = time.time()
         os.system("nodetool flush")
         t2 = time.time()
+        stalls = consume_stalls(logf)
+        print("Done.")
 
         duration = (t2 - t1) * 1000.
         space = du(KS_DIR)
-        print("Done.\nTime: {:.3f} ms\nSpace: {}".format(duration, space))
-        return (duration, int(space))
-
-if len(sys.argv) < 2:
-    print("Requires data file name")
-    sys.exit(0)
-
-if len(sys.argv) < 3:
-    print("Requires data file name and output file name")
-    sys.exit(0)
-
-infname = sys.argv[1]
-if not os.path.exists(infname):
-    print("File does not exist:", infname)
-    sys.exit(0)
-
-outfname = sys.argv[2]
+        return (duration, int(space), stalls)
 
 results = []
-for chunk in COMPR_CHUNKS:
-    print("Compression chunk length in kb:", chunk)
-    res_chunk = []
-    for cname, cdict in compressions(chunk):
-        print("Benchmarking compression:", cname)
-        t, s = bench_compr(infname, cdict)
-        res_chunk.append({'name': cname, 'flush_time': t, 'space': s})
-    results.append({'chunk_len': chunk, 'results': res_chunk})
+with open(logfname, 'r') as logf:
+    logf.seek(0, 2)
+    for chunk in COMPR_CHUNKS:
+        print("Compression chunk length in kb:", chunk)
+        res_chunk = []
+        for cname, cdict in compressions(chunk):
+            print("Benchmarking compression:", cname)
+            t, space, stalls = bench_compr(infname, cdict, logf)
+
+            mean_stall = stats.mean(stalls) if stalls else 0
+            stdev_stall = stats.stdev(stalls) if len(stalls) > 1 else 0
+
+            res_chunk.append({'name': cname, 'flush_time': t, 'space': space, 'stalls': len(stalls), 'mean_stall': mean_stall, 'stall_stdev': stdev_stall})
+
+            print("Time: {:.3f} ms\nSpace: {}\nReactor stalls: {}, stall mean: {} ms, stall stdev: {} ms".format(t, space, len(stalls), mean_stall, stdev_stall))
+        results.append({'chunk_len': chunk, 'results': res_chunk})
 
 print("results:\n", results)
 with open(outfname, 'w') as outf:
